698333482796e981f0faa919fa3370a226552359
GiuliaBertazzini <48946573+GiuliaBertazzini@users.noreply.github.com>
2020-09-15 16:21:19 +0200

    add virtual methods

--- Observer.h
+++ Observer.h
@@ -5,4 +5,10 @@
 #ifndef PROGRAMMAZIONE_OBSERVER_H
 #define PROGRAMMAZIONE_OBSERVER_H
 
+class Observer {
+public:
+    virtual void update() = 0;
+    virtual ~Observer(){};
+};
+
 #endif //PROGRAMMAZIONE_OBSERVER_H
--- Subject.h
+++ Subject.h
@@ -5,4 +5,15 @@
 #ifndef PROGRAMMAZIONE_SUBJECT_H
 #define PROGRAMMAZIONE_SUBJECT_H
 
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual void attachObserver(Observer *obs) = 0;
+    virtual void detachObserver(Observer *obs) = 0;
+    virtual void notifyObserver()  = 0;
+    virtual ~Subject() {};
+
+};
+
 #endif //PROGRAMMAZIONE_SUBJECT_H
