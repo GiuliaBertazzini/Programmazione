518c56ad3c75bdd38ac58282f354faeba9a25ff6
GiuliaBertazzini <48946573+GiuliaBertazzini@users.noreply.github.com>
2020-09-15 17:42:09 +0200

    add class to load resources

---
+++ LoadResources.cpp
@@ -0,0 +1,5 @@
+//
+// Created by giulia on 15/09/20.
+//
+
+#include "LoadResources.h"
---
+++ LoadResources.h
@@ -0,0 +1,22 @@
+//
+// Created by giulia on 15/09/20.
+//
+
+#ifndef PROGRAMMAZIONE_LOADRESOURCES_H
+#define PROGRAMMAZIONE_LOADRESOURCES_H
+
+#include "Subject.h"
+#include "Observer.h"
+
+class LoadResources : public Subject {
+public:
+    LoadResources();
+
+    virtual void attachObserver(Observer * obs) override;
+    virtual void detachObserver(Observer *obs) override;
+    virtual void notifyObserver() override;
+
+private:
+ //list <Observer*> observers;
+};
+#endif //PROGRAMMAZIONE_LOADRESOURCES_H
