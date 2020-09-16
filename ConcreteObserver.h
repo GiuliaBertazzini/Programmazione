6570aaf51b86976c492aa677e21eb9e2ae1de5ef
GiuliaBertazzini <48946573+GiuliaBertazzini@users.noreply.github.com>
2020-09-16 13:21:56 +0200

    add class

---
+++ ConcreteObserver.h
@@ -0,0 +1,30 @@
+//
+// Created by giulia on 15/09/20.
+//
+
+#ifndef PROGRAMMAZIONE_CONCRETEOBSERVER_H
+#define PROGRAMMAZIONE_CONCRETEOBSERVER_H
+
+#include "Observer.h"
+#include "LoadResources.h"
+
+class ConcreteObserver : public Observer {
+public:
+    ConcreteObserver(LoadResources *resources){
+        res=resources;
+        resources -> attachObserver(this);
+    }
+
+    virtual ~ConcreteObserver(){
+        res -> detachObserver(this);
+    }
+
+    virtual void update() {
+
+    }
+
+private:
+    LoadResources * res;
+};
+
+#endif //PROGRAMMAZIONE_CONCRETEOBSERVER_H
