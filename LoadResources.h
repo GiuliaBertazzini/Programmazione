b1ff316c3a40c852a16016d5119cae692796dcda
GiuliaBertazzini <48946573+GiuliaBertazzini@users.noreply.github.com>
2020-09-16 13:20:45 +0200

    add attribute

--- LoadResources.h
+++ LoadResources.h
@@ -7,16 +7,17 @@
 
 #include "Subject.h"
 #include "Observer.h"
+#include <list>
+using namespace std;
 
 class LoadResources : public Subject {
 public:
     LoadResources();
-
     virtual void attachObserver(Observer * obs) override;
     virtual void detachObserver(Observer *obs) override;
     virtual void notifyObserver() override;
 
 private:
- //list <Observer*> observers;
+    list<Observer*> observers;
 };
 #endif //PROGRAMMAZIONE_LOADRESOURCES_H
